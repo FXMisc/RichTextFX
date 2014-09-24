599a5782e8eef85c49177443019e3f56fcae14bd
Tomas Mikula <tomas.mikula@gmail.com>
2014-09-24 22:25:48 +0200

    Remove WellBehaved sources, add WellBehaved as dependency.

--- richtextfx/src/main/java/org/fxmisc/wellbehaved/input/InputReceiver.java
+++
@@ -1,9 +0,0 @@
-package org.fxmisc.wellbehaved.input;
-
-import javafx.event.EventHandler;
-import javafx.scene.input.InputEvent;
-
-public interface InputReceiver {
-    EventHandler<? super InputEvent> getOnInput();
-    void setOnInput(EventHandler<? super InputEvent> handler);
-}
