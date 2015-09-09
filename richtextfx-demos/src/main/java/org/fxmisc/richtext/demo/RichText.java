9c6d412916ec1d4bc2245b7557dcf031ef6f07f9
Tomas Mikula <tomas.mikula@gmail.com>
2015-09-09 11:27:05 -0400

    Add codec for StyleInfo in RichText demo.
    Enables copy & paste with style in the demo.

--- richtextfx-demos/src/main/java/org/fxmisc/richtext/demo/RichText.java
+++ richtextfx-demos/src/main/java/org/fxmisc/richtext/demo/RichText.java
@@ -6,6 +6,10 @@
 
 package org.fxmisc.richtext.demo;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.nio.charset.MalformedInputException;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Function;
@@ -27,6 +31,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
+import org.fxmisc.richtext.Codec;
 import org.fxmisc.richtext.InlineStyleTextArea;
 import org.fxmisc.richtext.StyleSpans;
 import org.reactfx.SuspendableNo;
@@ -34,7 +39,125 @@ import org.reactfx.SuspendableNo;
 public class RichText extends Application {
 
     private static class StyleInfo {
+
         public static final StyleInfo EMPTY = new StyleInfo();
+
+        public static final Codec<StyleInfo> CODEC = new Codec<StyleInfo>() {
+
+            private final Codec<Color> COLOR_CODEC = new Codec<Color>() {
+
+                @Override
+                public String getName() {
+                    return "color";
+                }
+
+                @Override
+                public void encode(DataOutputStream os, Color c)
+                        throws IOException {
+                    os.writeDouble(c.getRed());
+                    os.writeDouble(c.getGreen());
+                    os.writeDouble(c.getBlue());
+                    os.writeDouble(c.getOpacity());
+                }
+
+                @Override
+                public Color decode(DataInputStream is) throws IOException {
+                    return Color.color(
+                            is.readDouble(),
+                            is.readDouble(),
+                            is.readDouble(),
+                            is.readDouble());
+                }
+
+            };
+
+            @Override
+            public String getName() {
+                return "style-info";
+            }
+
+            @Override
+            public void encode(DataOutputStream os, StyleInfo s)
+                    throws IOException {
+                os.writeByte(encodeBoldItalicUnderlineStrikethrough(s));
+                os.writeInt(encodeOptionalUint(s.fontSize));
+                encodeOptional(os, s.fontFamily, Codec.STRING_CODEC);
+                encodeOptional(os, s.textColor, COLOR_CODEC);
+                encodeOptional(os, s.backgroundColor, COLOR_CODEC);
+            }
+
+            @Override
+            public StyleInfo decode(DataInputStream is) throws IOException {
+                byte bius = is.readByte();
+                Optional<Integer> fontSize = decodeOptionalUint(is.readInt());
+                Optional<String> fontFamily = decodeOptional(is, Codec.STRING_CODEC);
+                Optional<Color> textColor = decodeOptional(is, COLOR_CODEC);
+                Optional<Color> bgrColor = decodeOptional(is, COLOR_CODEC);
+                return new StyleInfo(
+                        bold(bius), italic(bius), underline(bius), strikethrough(bius),
+                        fontSize, fontFamily, textColor, bgrColor);
+            }
+
+            private int encodeBoldItalicUnderlineStrikethrough(StyleInfo s) {
+                return encodeOptionalBoolean(s.bold) << 6 |
+                       encodeOptionalBoolean(s.italic) << 4 |
+                       encodeOptionalBoolean(s.underline) << 2 |
+                       encodeOptionalBoolean(s.strikethrough);
+            }
+
+            private Optional<Boolean> bold(byte bius) throws IOException {
+                return decodeOptionalBoolean((bius >> 6) & 3);
+            }
+
+            private Optional<Boolean> italic(byte bius) throws IOException {
+                return decodeOptionalBoolean((bius >> 4) & 3);
+            }
+
+            private Optional<Boolean> underline(byte bius) throws IOException {
+                return decodeOptionalBoolean((bius >> 2) & 3);
+            }
+
+            private Optional<Boolean> strikethrough(byte bius) throws IOException {
+                return decodeOptionalBoolean((bius >> 0) & 3);
+            }
+
+            private int encodeOptionalBoolean(Optional<Boolean> ob) {
+                return ob.map(b -> 2 + (b ? 1 : 0)).orElse(0);
+            }
+
+            private Optional<Boolean> decodeOptionalBoolean(int i) throws IOException {
+                switch(i) {
+                    case 0: return Optional.empty();
+                    case 2: return Optional.of(false);
+                    case 3: return Optional.of(true);
+                }
+                throw new MalformedInputException(0);
+            }
+
+            private int encodeOptionalUint(Optional<Integer> oi) {
+                return oi.orElse(-1);
+            }
+
+            private Optional<Integer> decodeOptionalUint(int i) {
+                return (i < 0) ? Optional.empty() : Optional.of(i);
+            }
+
+            private <T> void encodeOptional(DataOutputStream os, Optional<T> ot, Codec<T> codec) throws IOException {
+                if(ot.isPresent()) {
+                    os.writeBoolean(true);
+                    codec.encode(os, ot.get());
+                } else {
+                    os.writeBoolean(false);
+                }
+            }
+
+            private <T> Optional<T> decodeOptional(DataInputStream is, Codec<T> codec) throws IOException {
+                return is.readBoolean()
+                        ? Optional.of(codec.decode(is))
+                        : Optional.empty();
+            }
+        };
+
         public static StyleInfo fontSize(int fontSize) { return EMPTY.updateFontSize(fontSize); }
         public static StyleInfo fontFamily(String family) { return EMPTY.updateFontFamily(family); }
         public static StyleInfo textColor(Color color) { return EMPTY.updateTextColor(color); }
@@ -221,6 +344,7 @@ public class RichText extends Application {
                     style -> style.toCss());
     {
         area.setWrapText(true);
+        area.setStyleCodec(StyleInfo.CODEC);
     }
 
     private final SuspendableNo updatingToolbar = new SuspendableNo();
