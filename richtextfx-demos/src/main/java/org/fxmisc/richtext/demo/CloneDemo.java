b134a160ec842635aabd366bf6b5fe553f05a7e2
JordanMartinez <jordanalex.martinez@gmail.com>
2015-12-12 20:06:09 -0800

    Revised CloneDemo to demonstrate User Interaction changes and programmatically-made changes.

--- richtextfx-demos/src/main/java/org/fxmisc/richtext/demo/CloneDemo.java
+++ richtextfx-demos/src/main/java/org/fxmisc/richtext/demo/CloneDemo.java
@@ -2,7 +2,11 @@ package org.fxmisc.richtext.demo;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.fxmisc.richtext.AreaFactory;
@@ -19,27 +23,62 @@ public class CloneDemo extends Application {
     public void start(Stage primaryStage) {
         String text = "Edit this area and watch the below area update itself.";
         InlineCssTextArea area = AreaFactory.inlineCssTextArea(text);
-        StyledTextArea<String, String> clone = area.createClone();
-
-        printAreaCarePosition("area", area);
-        printAreaCarePosition("clone", clone);
-
-        area.positionCaret(area.getLength());
-        printAreaCarePosition("area", area);
-        printAreaCarePosition("clone", clone);
-
-        clone.replaceText(0, clone.getLength(), "");
-        printAreaCarePosition("area", area);
-        printAreaCarePosition("clone", clone);
-
-        area.replaceText(0, 0, text);
-        printAreaCarePosition("area", area);
-        printAreaCarePosition("clone", clone);
+        StyledTextArea<String, String> clone = AreaFactory.cloneInlineCssTextArea(area);
 
         VBox vbox = new VBox(area, clone);
         vbox.setSpacing(10);
-        StackPane pane = new StackPane(vbox);
-        Scene scene = new Scene(pane, 400, 400);
+
+        // set up labels displaying caret position
+        String caret = "Caret: ";
+        Label areaCaret = new Label(caret);
+        area.caretPositionProperty().addListener((observable, oldValue, newValue) -> {
+            areaCaret.setText(caret + String.valueOf(newValue));
+        });
+        Label cloneCaret = new Label(caret);
+        clone.caretPositionProperty().addListener((observable, oldValue, newValue) -> {
+            cloneCaret.setText(caret + String.valueOf(newValue));
+        });
+
+        // set up label's displaying selection position
+        String selText = "Selected Text: ";
+        Label areaSelection = new Label(selText);
+        area.selectedTextProperty().addListener(((observable, oldValue, newValue) -> {
+            areaSelection.setText(selText + newValue);
+        }));
+        Label cloneSelection = new Label(selText);
+        clone.selectedTextProperty().addListener(((observable, oldValue, newValue) -> {
+            cloneSelection.setText(selText + newValue);
+        }));
+
+        // set up Label's distinguishing which labels belong to which area.
+        Label areaLabel = new Label("Original Area: ");
+        Label cloneLabel = new Label("Cloned Area: ");
+
+        // set up Buttons that programmatically change area but not clone
+        Button deleteLastThreeChars = new Button("Click to Delete the previous 3 chars.");
+        deleteLastThreeChars.setOnAction((ae) -> {
+            for (int i = 0; i <= 2; i++) {
+                area.deletePreviousChar();
+            }
+        });
+
+        // finish GUI
+        GridPane grid = new GridPane();
+        grid.add(areaLabel, 0, 0);
+        grid.add(areaCaret, 1, 0);
+        grid.add(areaSelection, 2, 0);
+        grid.add(cloneLabel, 0, 1);
+        grid.add(cloneCaret, 1, 1);
+        grid.add(cloneSelection, 2, 1);
+        grid.setHgap(10);
+        grid.setVgap(4);
+
+        BorderPane pane = new BorderPane();
+        pane.setCenter(vbox);
+        pane.setTop(grid);
+        pane.setBottom(deleteLastThreeChars);
+
+        Scene scene = new Scene(pane, 800, 500);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
