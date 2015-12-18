67581119c7a6b87175e6597cb019c41f310c0159
JordanMartinez <jordanalex.martinez@gmail.com>
2015-12-17 16:21:35 -0800

    Removed unneeded import in demo

--- richtextfx-demos/src/main/java/org/fxmisc/richtext/demo/CloneDemo.java
+++ richtextfx-demos/src/main/java/org/fxmisc/richtext/demo/CloneDemo.java
@@ -6,7 +6,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.fxmisc.richtext.AreaFactory;
