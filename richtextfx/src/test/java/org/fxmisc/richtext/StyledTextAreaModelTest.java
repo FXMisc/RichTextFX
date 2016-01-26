b017d83dc41a856f6d9b47b07b60919693851e58
JordanMartinez <jordanalex.martinez@gmail.com>
2016-01-25 21:14:54 -0800

    Added test: insure clones undo/redo methods work whether initial StyledTextAreaModel has been disposed or not.

--- richtextfx/src/test/java/org/fxmisc/richtext/StyledTextAreaModelTest.java
+++ richtextfx/src/test/java/org/fxmisc/richtext/StyledTextAreaModelTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.util.Collection;
 import java.util.Collections;
 
+import javafx.scene.control.IndexRange;
 import org.junit.Test;
 
 public class StyledTextAreaModelTest {
@@ -46,6 +47,42 @@ public class StyledTextAreaModelTest {
         // testing that undo/redo don't throw an exception
         model.undo();
         model.redo();
+    }
+
+    @Test
+    public void testClonesForUndoAndRedo() {
+        // set up an are with some styled content
+        StyledTextAreaModel<String, String> initialModel = new StyledTextAreaModel<>(
+                "", "", new EditableStyledDocument<>("", ""), true);
+        String textStyle = "-fx-font-size: 30px;";
+
+        String beforeText = "A really long ";
+        String styledText = "text example just ";
+        String afterText = "for kicks.";
+        initialModel.replaceText(beforeText + styledText + afterText);
+        initialModel.setStyle(beforeText.length(), beforeText.length() + styledText.length(), textStyle);
+
+        // create clones of initial model
+        StyledTextAreaModel<String, String> clone1 = new StyledTextAreaModel<>(
+                initialModel.getInitialParagraphStyle(), initialModel.getInitialStyle(),
+                initialModel.getContent(), initialModel.getUndoManagerWrapper(),
+                initialModel.isPreserveStyle()
+        );
+        StyledTextAreaModel<String, String> clone2 = new StyledTextAreaModel<>(
+                initialModel.getInitialParagraphStyle(), initialModel.getInitialStyle(),
+                initialModel.getContent(), initialModel.getUndoManagerWrapper(),
+                initialModel.isPreserveStyle()
+        );
+
+        // call undo on initialModel and dispose of it
+        initialModel.undo();
+        initialModel.dispose();
+
+        // insure that redo on clones still works
+        clone1.redo();
+        clone2.undo();
+        clone1.dispose();
+        clone2.redo();
     }
 
 }
