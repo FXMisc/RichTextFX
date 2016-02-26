3f56e77ce12734de3d332e1d4123ee7cd7bdbf11
JordanMartinez <jordanalex.martinez@gmail.com>
2016-02-26 10:33:26 -0800

    Updated javadoc: either CodeAreaModel or its superclass can be used for CodeArea

--- richtextfx/src/main/java/org/fxmisc/richtext/CodeArea.java
+++ richtextfx/src/main/java/org/fxmisc/richtext/CodeArea.java
@@ -5,6 +5,10 @@ package org.fxmisc.richtext;
  * A convenience subclass of {@link StyleClassedTextArea}
  * with fixed-width font and an undo manager that observes
  * only plain text changes (not styled changes).
+ *
+ * <p>Note: either {@link CodeAreaModel} or {@link StyleClassedTextAreaModel} can be used with this view.
+ * {@code CodeAreaModel} only provides some extra constructors to use in {@link AreaFactory}, such as
+ * {@link AreaFactory#codeArea(String)}</p>.
  */
 public class CodeArea<Model extends StyleClassedTextAreaModel> extends StyleClassedTextArea<Model> {
 
