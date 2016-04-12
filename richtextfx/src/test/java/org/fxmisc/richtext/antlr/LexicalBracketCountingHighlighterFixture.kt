dec6f83c524762adb5d6228b1d5dc6d8b5adb4d0
Geoff-Laptop <geoff_groos@msn.com>
2016-04-12 00:29:21 -0700

    wrote a single test, after a great deal of work

--- richtextfx/src/test/java/org/fxmisc/richtext/antlr/LexicalBracketCountingHighlighterFixture.kt
+++ richtextfx/src/test/java/org/fxmisc/richtext/antlr/LexicalBracketCountingHighlighterFixture.kt
@@ -1,5 +1,8 @@
+
 package org.fxmisc.richtext.antlr
 
+import org.assertj.guava.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
 /**
@@ -7,9 +10,23 @@ import org.junit.Test
  */
 class LexicalBracketCountingHighlighterFixture {
 
-    @Test fun when_stuff(){
-        var listener = LexicalBracketCountingHighlighter("", "", "")
+    @Test fun when_highlighting_a_selected_bracket_should_properly_highlight_corresponding_bracket(){
 
-        listener.generateNewStyles()
+        //setup
+        val listener = LexicalBracketCountingHighlighter("(", ")", "bracket")
+        val textArea = StructuredTextArea(
+                "org.fxmisc.richtext.parser.TestLangParser",
+                "org.fxmisc.richtext.parser.TestLangLexer",
+                "body"
+        )
+        //indexes                                     |18  |23
+        textArea.replaceText(0, 0, "BracketHell: (()()(()()))")
+
+        //act
+        val styles = listener.generateNewStyles(textArea, textArea.tokensByCharIndex)
+
+        //assert
+        assertThat(styles.get(18)).isEqualTo("bracket");
+        assertThat(styles.get(23)).isEqualTo("bracket");
     }
 }
