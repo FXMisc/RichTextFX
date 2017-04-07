149c2374e2ec473b759f9979ad27bf76a2b55fc3
Jordan Martinez <jordanalex.martinez@gmail.com>
2017-04-06 19:50:07 -0700

    Fix regression: changing empty paragraph's paragraph style is valid change

--- richtextfx/src/main/java/org/fxmisc/richtext/model/PlainTextChange.java
+++ richtextfx/src/main/java/org/fxmisc/richtext/model/PlainTextChange.java
@@ -3,9 +3,26 @@ package org.fxmisc.richtext.model;
 
 public class PlainTextChange extends TextChange<String, PlainTextChange> {
 
+    private final ChangeType type;
+
     public PlainTextChange(int position, String removed, String inserted) {
         super(position, removed, inserted);
+        if (insertedLength() == 0) {
+            if (removedLength() == 0) {
+                throw new IllegalStateException(String.format("Cannot get the type of a change that neither inserts nor deletes anything." +
+                        "removed=%s inserted=%s", removed, inserted));
+            } else {
+                type = ChangeType.DELETION;
+            }
+        } else if (removedLength() == 0) {
+            type = ChangeType.INSERTION;
+        } else {
+            type = ChangeType.REPLACEMENT;
+        }
     }
+
+    @Override
+    public final ChangeType getType() { return type; }
 
     @Override
     protected int removedLength() {
