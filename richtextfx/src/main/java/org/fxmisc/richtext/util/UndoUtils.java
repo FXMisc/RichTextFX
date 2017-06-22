ce27fd34083cb47f95891f6e0d619b2c7bf33fca
Jordan Martinez <jordanalex.martinez@gmail.com>
2017-06-21 20:28:37 -0700

    Add javadoc to UndoUtils

--- richtextfx/src/main/java/org/fxmisc/richtext/util/UndoUtils.java
+++ richtextfx/src/main/java/org/fxmisc/richtext/util/UndoUtils.java
@@ -9,12 +9,20 @@ import org.fxmisc.undo.UndoManagerFactory;
 
 import java.util.function.Consumer;
 
+/**
+ * A class filled with factory methods to help easily construct an {@link UndoManager} for a {@link GenericStyledArea}.
+ */
 public final class UndoUtils {
 
     private UndoUtils() {
         throw new IllegalStateException("UndoUtils cannot be instantiated");
     }
 
+    /**
+     * Constructs an UndoManager with an unlimited history:
+     * if {@link GenericStyledArea#isPreserveStyle() the area's preserveStyle flag is true}, the returned UndoManager
+     * can undo/redo {@link RichTextChange}s; otherwise, it can undo/redo {@link PlainTextChange}s.
+     */
     public static <PS, SEG, S> UndoManager defaultUndoManager(GenericStyledArea<PS, SEG, S> area) {
         return area.isPreserveStyle()
                 ? richTextUndoManager(area)
@@ -29,22 +37,34 @@ public final class UndoUtils {
      *                                                                        *
      * ********************************************************************** */
 
+    /**
+     * Returns an UndoManager with an unlimited history that can undo/redo {@link RichTextChange}s.
+     */
     public static <PS, SEG, S> UndoManager<RichTextChange<PS, SEG, S>> richTextUndoManager(GenericStyledArea<PS, SEG, S> area) {
         return richTextUndoManager(area, UndoManagerFactory.unlimitedHistoryFactory());
     }
 
+    /**
+     * Returns an UndoManager that can undo/redo {@link RichTextChange}s.
+     */
     public static <PS, SEG, S> UndoManager<RichTextChange<PS, SEG, S>> richTextUndoManager(GenericStyledArea<PS, SEG, S> area,
                                                                UndoManagerFactory factory) {
-        return factory.create(area.richChanges(), RichTextChange::invert, applyRichTextChange(area), TextChange::mergeWith, TextChange::isIdentity);
+        return factory.create(area.richChanges(), TextChange::invert, applyRichTextChange(area), TextChange::mergeWith, TextChange::isIdentity);
     };
 
+    /**
+     * Returns an UndoManager with an unlimited history that can undo/redo {@link PlainTextChange}s.
+     */
     public static <PS, SEG, S> UndoManager<PlainTextChange> plainTextUndoManager(GenericStyledArea<PS, SEG, S> area) {
         return plainTextUndoManager(area, UndoManagerFactory.unlimitedHistoryFactory());
     }
 
+    /**
+     * Returns an UndoManager that can undo/redo {@link PlainTextChange}s.
+     */
     public static <PS, SEG, S> UndoManager<PlainTextChange> plainTextUndoManager(GenericStyledArea<PS, SEG, S> area,
                                                                 UndoManagerFactory factory) {
-        return factory.create(area.plainTextChanges(), PlainTextChange::invert, applyPlainTextChange(area), TextChange::mergeWith, TextChange::isIdentity);
+        return factory.create(area.plainTextChanges(), TextChange::invert, applyPlainTextChange(area), TextChange::mergeWith, TextChange::isIdentity);
     }
 
     /* ********************************************************************** *
@@ -55,11 +75,19 @@ public final class UndoUtils {
      *                                                                        *
      * ********************************************************************** */
 
+    /**
+     * Applies a {@link PlainTextChange} to the given area when the {@link UndoManager}'s change stream emits an event
+     * by {@code area.replaceText(change.getPosition(), change.getRemovalEnd(), change.getInserted()}.
+     */
     public static <PS, SEG, S> Consumer<PlainTextChange> applyPlainTextChange(GenericStyledArea<PS, SEG, S> area) {
-        return change -> area.replaceText(change.getPosition(), change.getPosition() + change.getRemoved().length(), change.getInserted());
+        return change -> area.replaceText(change.getPosition(), change.getRemovalEnd(), change.getInserted());
     }
 
+    /**
+     * Applies a {@link PlainTextChange} to the given area when the {@link UndoManager}'s change stream emits an event
+     * by {@code area.replace(change.getPosition(), change.getRemovalEnd(), change.getInserted()}.
+     */
     public static <PS, SEG, S> Consumer<RichTextChange<PS, SEG, S>> applyRichTextChange(GenericStyledArea<PS, SEG, S> area) {
-        return change -> area.replace(change.getPosition(), change.getPosition() + change.getRemoved().length(), change.getInserted());
+        return change -> area.replace(change.getPosition(), change.getRemovalEnd(), change.getInserted());
     }
 }
