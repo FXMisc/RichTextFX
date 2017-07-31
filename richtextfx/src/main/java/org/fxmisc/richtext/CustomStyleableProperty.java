329e3c20cf33e05f6272460d2ae10881de91eaab
Jordan Martinez <jordanalex.martinez@gmail.com>
2017-07-31 14:50:39 -0700

    Extract CustomStyleableProperty to own class; remove subclasses

---
+++ richtextfx/src/main/java/org/fxmisc/richtext/CustomStyleableProperty.java
@@ -0,0 +1,36 @@
+package org.fxmisc.richtext;
+
+import javafx.css.CssMetaData;
+import javafx.css.Styleable;
+import javafx.css.StyleableObjectProperty;
+
+public class CustomStyleableProperty<T> extends StyleableObjectProperty<T> {
+
+    private final Object bean;
+    private final String name;
+    private final CssMetaData<? extends Styleable, T> cssMetaData;
+
+    public CustomStyleableProperty(T initialValue, String name, Object bean,
+                                   CssMetaData<? extends Styleable, T> cssMetaData) {
+        super(initialValue);
+        this.bean = bean;
+        this.name = name;
+        this.cssMetaData = cssMetaData;
+    }
+
+    @Override
+    public Object getBean() {
+        return bean;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public CssMetaData<? extends Styleable, T> getCssMetaData() {
+        return cssMetaData;
+    }
+
+}
