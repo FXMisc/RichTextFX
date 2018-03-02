93aa80a71761b23b32d32a768f60249ca08e4c93
Jurgen <admedfx@gmail.com>
2018-03-02 11:03:44 +0200

    Test as supplied by JFormDesigner

--- richtextfx/src/test/java/org/fxmisc/richtext/model/ParagraphTest.java
+++ richtextfx/src/test/java/org/fxmisc/richtext/model/ParagraphTest.java
@@ -2,6 +2,10 @@ package org.fxmisc.richtext.model;
 
 import static org.junit.Assert.*;
 
+import java.util.Collection;
+import java.util.Collections;
+
+import org.fxmisc.richtext.StyleClassedTextArea;
 import org.junit.Test;
 
 public class ParagraphTest {
@@ -37,4 +41,19 @@ public class ParagraphTest {
         assertTrue(restyledP.getStyleSpans().styleStream().allMatch( b -> b ));
     }
 
+    // Relates to #696 (caused by #685, coming from #449) where an empty
+    // style being applied to an empty paragraph results in an Exception
+    @Test
+    public void restylingEmptyParagraphViaEmptyStyleSpansWorks() {
+
+        StyleSpansBuilder<Collection<String>> spansBuilder = new StyleSpansBuilder<>();
+        spansBuilder.add(Collections.singleton("cls1"), 1);
+        spansBuilder.add(Collections.singleton("cls2"), 3);
+        spansBuilder.add(Collections.emptyList(), 1);		// critical trigger
+        StyleSpans ss = spansBuilder.create();
+
+        StyleClassedTextArea area = new StyleClassedTextArea();
+        area.replaceText("* a\n\n");
+        area.setStyleSpans(0, ss);							// would fail here
+    }
 }
