3a99ca5049c7c9ff1f4d7b25552827b831a09f86
Jurgen <admedfx@gmail.com>
2020-01-07 14:00:35 +0200

    Added styled text fields

---
+++ richtextfx/src/main/java/org/fxmisc/richtext/InlineCssTextField.java
@@ -0,0 +1,23 @@
+package org.fxmisc.richtext;
+
+import org.fxmisc.richtext.model.SimpleEditableStyledDocument;
+
+import javafx.scene.text.TextFlow;
+
+/**
+ * A TextField that uses inline CSS, i.e. <code>setStyle(String)</code>, to define the styles of text segments.
+ * <p>Use CSS Style Class ".styled-text-field" for styling the control.
+ * @author Jurgen
+ */
+public class InlineCssTextField extends StyledTextField<String,String>
+{
+    public InlineCssTextField() {
+        super( "", TextFlow::setStyle, "", TextExt::setStyle, new SimpleEditableStyledDocument<>("", "") );
+    }
+    
+    public InlineCssTextField( String text ) {
+        this(); replaceText( text );
+        getUndoManager().forgetHistory();
+        getUndoManager().mark();
+    }
+}
