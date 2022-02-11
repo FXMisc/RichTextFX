6581c9b3515188c54fd712b39ed4c9e3461756eb
Jurgen <jurgen@ivory.web.za>
2022-02-11 12:42:12 +0200

    Override CodeArea navigation with RegEx implementation

--- richtextfx/src/main/java/org/fxmisc/richtext/CodeArea.java
+++ richtextfx/src/main/java/org/fxmisc/richtext/CodeArea.java
@@ -1,6 +1,8 @@
 package org.fxmisc.richtext;
 
 import java.util.Collection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javafx.beans.NamedArg;
 import org.fxmisc.richtext.model.EditableStyledDocument;
@@ -50,5 +52,82 @@ public class CodeArea extends StyleClassedTextArea {
 
         // position the caret at the beginning
         selectRange(0, 0);
+    }
+
+    protected Pattern WORD_PATTERN = Pattern.compile( "\\w+" );
+    protected Pattern WORD_SPACE = Pattern.compile( "\\w*\\h*" );
+
+    @Override
+    public void wordBreaksBackwards(int n, SelectionPolicy selectionPolicy)
+    {
+        if ( getLength() == 0 ) return;
+
+        CaretSelectionBind<?,?,?> csb = getCaretSelectionBind();
+        int paragraph = csb.getParagraphIndex();
+        int position = csb.getColumnPosition(); 
+        int prevWord = 0;
+
+        if ( position == 0 ) {
+            prevWord = getParagraph( --paragraph ).length();
+            moveTo( paragraph, prevWord, selectionPolicy );
+            return;
+        }
+        
+        Matcher m = WORD_SPACE.matcher( getText( paragraph ) );
+        
+        while ( m.find() )
+        {
+            if ( m.start() == position ) {
+                moveTo( paragraph, prevWord, selectionPolicy );
+                break;
+            }
+            if ( (prevWord = m.end()) >= position ) {
+                moveTo( paragraph, m.start(), selectionPolicy );
+                break;
+            }
+        }
+    }
+    
+    @Override
+    public void wordBreaksForwards(int n, SelectionPolicy selectionPolicy)
+    {
+        if ( getLength() == 0 ) return;
+
+        CaretSelectionBind<?,?,?> csb = getCaretSelectionBind();
+        int paragraph = csb.getParagraphIndex();
+        int position = csb.getColumnPosition(); 
+        
+        Matcher m = WORD_SPACE.matcher( getText( paragraph ) );
+        
+        while ( m.find() )
+        {
+            if ( m.start() > position ) {
+                moveTo( paragraph, m.start(), selectionPolicy );
+                break;
+            }
+            if ( m.hitEnd() ) {
+                moveTo( paragraph+1, 0, selectionPolicy );
+            }
+        }
+    }
+    
+    @Override
+    public void selectWord()
+    {
+        if ( getLength() == 0 ) return;
+
+        CaretSelectionBind<?,?,?> csb = getCaretSelectionBind();
+        int paragraph = csb.getParagraphIndex();
+        int position = csb.getColumnPosition(); 
+        
+        Matcher m = WORD_PATTERN.matcher( getText( paragraph ) );
+
+        while ( m.find() )
+        {
+            if ( m.end() > position ) {
+                csb.selectRange( paragraph, m.start(), paragraph, m.end() );
+                return;
+            }
+        }
     }
 }
