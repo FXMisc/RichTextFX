d78bab9a3c1306ed3d1e91e5ebaa44e025db10cd
Sinsy <550569627@qq.com>
2024-06-25 15:33:07 +0800

    feat: line number highlight demo (#1200)
    * feat: line number highlight demo
    * Package and class name fix up
    * Removed unused numberFactory
    * Only add currentParagraphProperty listener once
    * Replace complex list usage with SimpleIntegerProperty
    * Make LineNumberHighLightFactory independent of outer class
    * Replaced double Label and setVisible with setStyle
    * Removed unneeded CodeArea field and StackPane
    * Removed unnecessary index increment
    * Replaced IntegerProperty with direct currentParagraphProperty use
    * Removed unneeded graphicFactory wrapper
    * Added implementation comment
    * Implemented more efficient style change mechanism
    * Fixed indent
    * Renamed variable
    * Moved to lineindicator folder
    ---------
    Co-authored-by: Jurgen <5031427+Jugen@users.noreply.github.com>

---
+++ richtextfx-demos/src/main/java/org/fxmisc/richtext/demo/lineindicator/LineNumberHighLightDemo.java
@@ -0,0 +1,66 @@
+package org.fxmisc.richtext.demo.lineindicator;
+
+import javafx.application.Application;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import org.fxmisc.richtext.CodeArea;
+
+import java.util.function.IntFunction;
+
+public class LineNumberHighLightDemo extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        CodeArea codeArea = new CodeArea();
+        codeArea.replaceText(0,0,"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        codeArea.setPrefHeight(600);
+
+        IntFunction<Node> labelFactory = new LineNumberHighLightFactory( codeArea );
+        codeArea.setParagraphGraphicFactory(labelFactory);
+
+        VBox vbox = new VBox();
+        vbox.getChildren().addAll(codeArea);
+
+        Scene scene = new Scene(vbox, 600, 600);
+        primaryStage.setTitle("Line number highLight Demo");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    class LineNumberHighLightFactory implements IntFunction<Node> {
+
+        private final ObservableValue<Integer> shownLines;
+
+        public LineNumberHighLightFactory( CodeArea codeArea ) {
+            shownLines = codeArea.currentParagraphProperty();
+            shownLines.addListener( (ob,oldValue,newValue) ->
+            {
+                codeArea.getParagraphGraphic( oldValue ).setStyle("-fx-text-fill: blue");
+                codeArea.getParagraphGraphic( newValue ).setStyle("-fx-text-fill: red");
+            });
+        }
+
+        @Override
+        public Node apply(int lineIndex) {
+
+            Label after = new Label();
+            after.setText(String.valueOf(lineIndex+1));
+
+            if ( lineIndex != shownLines.getValue() ) {
+                after.setStyle("-fx-text-fill: blue");
+            }
+            else after.setStyle("-fx-text-fill: red");
+
+            return after;
+        }
+    }
+}
