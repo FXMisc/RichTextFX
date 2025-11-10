d859cbe5ef7bf4f3d03c68592df26a9632b30c47
Symeon94 <symeon.malengreau@squareupsoft.com>
2025-11-10 13:16:04 +0100

    Issue #1293: Removing caret position from change, because the change is part of the document and the document has no knowledge of user interaction, something must wrap the caret change somewhere where the change is being called

--- richtextfx/src/main/java/org/fxmisc/richtext/model/RichTextChange.java
+++ richtextfx/src/main/java/org/fxmisc/richtext/model/RichTextChange.java
@@ -4,26 +4,21 @@ package org.fxmisc.richtext.model;
  * An object that specifies where a change occurred in a {@link org.fxmisc.richtext.GenericStyledArea}.
  */
 public class RichTextChange<PS, SEG, S> extends TextChange<RichTextChangeData<PS, SEG, S>, RichTextChange<PS, SEG, S>> {
-
-    public RichTextChange(int caretBefore, int position, StyledDocument<PS, SEG, S> removed, StyledDocument<PS, SEG, S> inserted) {
-        this(caretBefore, position, new RichTextChangeData<>(removed), new RichTextChangeData<>(inserted));
+    public RichTextChange(int position, StyledDocument<PS, SEG, S> removed, StyledDocument<PS, SEG, S> inserted) {
+        this(position, new RichTextChangeData<>(removed), new RichTextChangeData<>(inserted));
     }
 
-    private RichTextChange(int caretBefore, int position, RichTextChangeData<PS, SEG, S> removed, RichTextChangeData<PS, SEG, S> inserted) {
-        super(caretBefore, position, removed, inserted);
-    }
-
-    private RichTextChange(CaretChange caretChange, int position, RichTextChangeData<PS, SEG, S> removed, RichTextChangeData<PS, SEG, S> inserted) {
-        super(caretChange, position, removed, inserted);
+    private RichTextChange(int position, RichTextChangeData<PS, SEG, S> removed, RichTextChangeData<PS, SEG, S> inserted) {
+        super(position, removed, inserted);
     }
 
     @Override
-    protected final RichTextChange<PS, SEG, S> create(CaretChange caretChange, int position, RichTextChangeData<PS, SEG, S> removed, RichTextChangeData<PS, SEG, S> inserted) {
-        return new RichTextChange<>(caretChange, position, removed, inserted);
+    protected final RichTextChange<PS, SEG, S> create(int position, RichTextChangeData<PS, SEG, S> removed, RichTextChangeData<PS, SEG, S> inserted) {
+        return new RichTextChange<>(position, removed, inserted);
     }
 
     public final PlainTextChange toPlainTextChange() {
-        return new PlainTextChange(getCaretChange().before(), getPosition(), getRemoved().getText(), getInserted().getText());
+        return new PlainTextChange(getPosition(), getRemoved().getText(), getInserted().getText());
     }
 
     /**
